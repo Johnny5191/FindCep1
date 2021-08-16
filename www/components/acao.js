27436acf3f49c639aaa69b700d7755ceda948767
Johnny5191 <souocara996@gmail.com>
2021-08-17 03:28:40 +0900

    Project's Initial Commit

---
+++ www/components/acao.js
@@ -0,0 +1,26 @@
+
+
+window.onload = function(){
+  const buscar = document.querySelector("#buscar");
+
+  const cep = document.querySelector("#cep");
+
+  const opcoes = {
+    method: 'GET',
+    mede: 'cors',
+    cache: 'default'
+  }
+  buscar.addEventListener("click", function(){
+   fetch(`http://viacep.com.br/ws/${ cep.value }/json/`, opcoes)
+      .then(response => {response.json() 
+      .then(data => {                  
+      
+        document.querySelector("#estado").value = data['uf'];
+        document.querySelector("#cidade").value = data['localidade'];
+        document.querySelector("#bairro").value = data['bairro'];
+        document.querySelector("#rua").value = data['logradouro'];
+        })
+      })      
+  });
+
+}
